9e95a0f4e9e76650299232b1f3e73e544de871dc
Oleksandr Shtovba <jilexandr@gmail.com>
2026-09-11 14:26:22 +0300

    Add project tooling and CI

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(process.cwd(), "widget/index.html")
+    }
+  }
+});
